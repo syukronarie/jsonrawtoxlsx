473cfc36faf99ca770390525af9d30e22dc0964b
M Arie Syukron <syukronarie@gmail.com>
2023-10-15 21:25:27 +0700

    Fix/snyk issue: updating xlsx (#41)
    * docs(lib): updated version package (#37)
    
    * fix: upgrade husky from 8.0.2 to 8.0.3 (#38)
    
    Snyk has created this PR to upgrade husky from 8.0.2 to 8.0.3.
    
    See this package in npm:
    
    
    See this project in Snyk:
    https://app.snyk.io/org/syukronarie/project/0439d359-51d1-4907-a17e-522230844cf7?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade husky from 8.0.2 to 8.0.3 (#39)
    
    Snyk has created this PR to upgrade husky from 8.0.2 to 8.0.3.
    
    See this package in npm:
    
    
    See this project in Snyk:
    https://app.snyk.io/org/syukronarie/project/0439d359-51d1-4907-a17e-522230844cf7?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * Fix/snyk issue: updating xlsx  (#40)
    
    * chore: updating code convention
    * chore: deleting unnecesarry file
    * chore: updating xlsx to fix snyk issue

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const XLSX = require('xlsx');
+const XLSX = require("xlsx");
 
-const jsontoxlsx = function (json) {
-  const workSheet = XLSX.utils.json_to_sheet(json);
-  const workBook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workBook, workSheet, 'sheet');
-  // Generate buffer
-  return XLSX.write(workBook, { bookType: 'xlsx', type: 'buffer' });
+const jsontoxlsx = (json) => {
+	const workSheet = XLSX.utils.json_to_sheet(json);
+	const workBook = XLSX.utils.book_new();
+	XLSX.utils.book_append_sheet(workBook, workSheet, "sheet");
+	// Generate buffer
+	return XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
 };
 
 jsontoxlsx.middleware = function (_, res, next) {
-  res.xlsx = function (fileName, json) {
-    const xlsx = jsontoxlsx(json);
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-    res.setHeader('Content-disposition', 'attachment;filename=' + fileName + '.xlsx');
-    res.end(xlsx, 'binary');
-  };
-  next();
+	res.xlsx = function (fileName, json) {
+		const xlsx = jsontoxlsx(json);
+		res.setHeader("Content-Type", "application/vnd.openxmlformats");
+		res.setHeader(
+			"Content-disposition",
+			"attachment;filename=" + fileName + ".xlsx"
+		);
+		res.end(xlsx, "binary");
+	};
+	next();
 };
 
 module.exports = jsontoxlsx;
